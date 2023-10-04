7e2a6abddbdd3c088147d02c1c74a434f31025f9
Tran Ba Khang(MS/EMC31-XC) <Khang.TranBa@vn.bosch.com>
2023-10-04 15:19:42 +0700

    ivi-controller: should not repaint the background view when destination is 0
    Signed-off-by: Tran Ba Khang(MS/EMC31-XC) <Khang.TranBa@vn.bosch.com>

--- weston-ivi-shell/src/ivi-controller.c
+++ weston-ivi-shell/src/ivi-controller.c
@@ -900,18 +900,21 @@ set_bkgnd_surface_prop(struct ivishell *shell)
     dest_rect.width = dest_width;
     dest_rect.height = dest_height;
 
-    calc_trans_matrix(&source_rect, &dest_rect,
-                      &shell->bkgnd_transform.matrix);
-    weston_matrix_translate(&shell->bkgnd_transform.matrix, x, y, 0.0f);
+    /*Only repaint when size of destination bigger than 0*/
+    if (dest_rect.width != 0 && dest_rect.height != 0) {
+        calc_trans_matrix(&source_rect, &dest_rect,
+                    &shell->bkgnd_transform.matrix);
+        weston_matrix_translate(&shell->bkgnd_transform.matrix, x, y, 0.0f);
 
-    weston_log("set_bkgnd_surface_prop: x:%d y:%d s_width:%d s_height:%d d_width:%d d_height:%d\n",
-               x, y, src_width, src_height, dest_width, dest_height);
+        weston_log("set_bkgnd_surface_prop: x:%d y:%d s_width:%d s_height:%d d_width:%d d_height:%d\n",
+                x, y, src_width, src_height, dest_width, dest_height);
 
-    wl_list_insert(&view->geometry.transformation_list,
-                   &shell->bkgnd_transform.link);
-    view->transform.dirty = 1;
-    weston_view_update_transform(view);
-    ivi_weston_surface_schedule_repaint(w_surface);
+        wl_list_insert(&view->geometry.transformation_list,
+                    &shell->bkgnd_transform.link);
+        view->transform.dirty = 1;
+        weston_view_update_transform(view);
+        ivi_weston_surface_schedule_repaint(w_surface);
+    }
 }
 
 static void
