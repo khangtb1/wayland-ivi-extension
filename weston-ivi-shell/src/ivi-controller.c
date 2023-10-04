632960f2489e702f4359d149e10a8eb92d3a50de
Tran Ba Khang(MS/EMC31-XC) <Khang.TranBa@vn.bosch.com>
2023-10-04 07:39:59 +0700

    ivi-controller: fix wrong pointer in output_resized event
    Signed-off-by: Tran Ba Khang(MS/EMC31-XC) <Khang.TranBa@vn.bosch.com>

--- weston-ivi-shell/src/ivi-controller.c
+++ weston-ivi-shell/src/ivi-controller.c
@@ -1510,7 +1510,7 @@ output_destroyed_event(struct wl_listener *listener, void *data)
 static void
 output_resized_event(struct wl_listener *listener, void *data)
 {
-    struct ivishell *shell = wl_container_of(listener, shell, output_destroyed);
+    struct ivishell *shell = wl_container_of(listener, shell, output_resized);
 
     if (shell->bkgnd_view && shell->client)
         set_bkgnd_surface_prop(shell);
